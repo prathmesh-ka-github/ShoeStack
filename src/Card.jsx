d4cec9f3661baeaf2de3224c8aef02969eb2645d
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-10-17 11:52:19 -0400

    rendering products in collections is now all working and set. next step is to work on individual product pages and add to cart function

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import './styles/card.css';
 
 function Card(props) {
-    var productimage = props.productimage
- 
+    const {id, productName, price, productImage} = props.data
     return (
         <div className="card-container">
             <div className="card-img">
-                <img src={`${productimage}`} alt="image"/>
+                <img src={productImage} alt="" />
             </div>
-            <div className="card-name">{props.name}</div>
+            <div className="card-name">{productName}</div>
+            <div className="card-price">{price}</div>
             <div className="buttons">
                 <button id="cart">Add to cart</button>
                 <button id="wishlist">Add to wishlist</button>
